0df218b69eb723d17bac614255b05a38f85c3129
cristiano <cristiano2132.ufv@gmail.com>
2025-08-05 14:42:11 -0300

    update pack version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="expdespy",
-    version="1.1.4",
+    version="1.0.0",
     description="Statistical analysis of experimental designs in Python",
     author="Seu Nome",
     packages=find_packages(where="src"),
