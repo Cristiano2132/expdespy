1dcc6b710f0bc5b412778dcd04e29ec4b7f77440
cristiano <cristiano2132.ufv@gmail.com>
2025-08-15 17:50:46 -0300

    add notebook exemplo

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ if ! docker build -t $IMAGE_NAME .; then
 fi
 
 echo "Running tests with coverage inside Docker container..."
-if ! docker run --rm $IMAGE_NAME; then
+if ! docker run --rm -e PYTHONPATH=/app/src $IMAGE_NAME; then
   echo "Tests failed or coverage less than 90%."
   exit 1
 fi
